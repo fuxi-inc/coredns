c3956d150c228c472c95a710df4db3a79c349ab4
jojopdq <jojopdq@gmail.com>
2020-09-30 13:15:09 +0800

    update the silly query logic

--- plugin/blockchain/blockchain.go
+++ plugin/blockchain/blockchain.go
@@ -16,6 +16,63 @@ type Blockchain struct {
 	IPFSGatewayAAAAs  []string
 }
 
+var domains = make(map[string][]dns.RR)
+
+func init() {
+	requestor := "requestor.data.heze."
+	requestorCert := dns.CERT{
+		Hdr: dns.RR_Header{
+			Name:     requestor,
+			Rrtype:   dns.TypeCERT,
+			Class:    dns.ClassINET,
+			Ttl:      108,
+			Rdlength: 100,
+		},
+		Type:        1,
+		KeyTag:      1,
+		Algorithm:   1,
+		Certificate: "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVA2gkONVS5y3W6cnEaERuPsGNqD8kG14+sWjoquToTC7dlAIdNZzF3+a+Ak+bWaHZqF0f+3rVh/bD7uFO4riRyuRZVha09riDPFbsAb1S8//20v59dk+cfx0LskbUzne8U4dS0MCAA8TLhI/wnjJ0KAOeCAC8S1dFt0GwckOSlwIDAQAB",
+	}
+	requestorRRs := make([]dns.RR, 0)
+	requestorRRs = append(requestorRRs, &requestorCert)
+	domains[requestor] = requestorRRs
+
+	dataProvider := "provider.data.heze."
+	dataProviderCert := dns.CERT{
+		Hdr: dns.RR_Header{
+			Name:     requestor,
+			Rrtype:   dns.TypeCERT,
+			Class:    dns.ClassINET,
+			Ttl:      108,
+			Rdlength: 100,
+		},
+		Type:        1,
+		KeyTag:      1,
+		Algorithm:   1,
+		Certificate: "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVA2gkONVS5y3W6cnEaERuPsGNqD8kG14+sWjoquToTC7dlAIdNZzF3+a+Ak+bWaHZqF0f+3rVh/bD7uFO4riRyuRZVha09riDPFbsAb1S8//20v59dk+cfx0LskbUzne8U4dS0MCAA8TLhI/wnjJ0KAOeCAC8S1dFt0GwckOSlwIDAQAB",
+	}
+	dataProviderRRs := make([]dns.RR, 0)
+	dataProviderRRs = append(dataProviderRRs, &dataProviderCert)
+	domains[dataProvider] = dataProviderRRs
+
+	dataService := "api.data.heze."
+	dataServiceUri := dns.URI{
+		Hdr: dns.RR_Header{
+			Name:     dataService,
+			Rrtype:   dns.TypeURI,
+			Class:    dns.ClassINET,
+			Ttl:      36,
+			Rdlength: 100,
+		},
+		Priority: 1,
+		Weight:   1,
+		Target:   "http://192.168.10.252:5000/auth?&response_type=code&redirect_uri=http://192.168.10.252:9009/v/1&client_id=did.fuxi.city/jinzhongze@fxzk.org",
+	}
+	dataServiceRRs := make([]dns.RR, 0)
+	dataServiceRRs = append(dataServiceRRs, &dataServiceUri)
+	domains[dataService] = dataServiceRRs
+}
+
 // ServeDNS implements the plugin.Handler interface.
 func (e Blockchain) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 	state := request.Request{W: w, Req: r}
@@ -51,38 +108,7 @@ func (e Blockchain) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.M
 func (e Blockchain) Name() string { return "blockchain" }
 
 func (e Blockchain) handleDomain(name string, domain string) ([]dns.RR, error) {
-	results := make([]dns.RR, 0)
-
-	uri := dns.URI{
-		Hdr: dns.RR_Header{
-			Name:     domain,
-			Rrtype:   dns.TypeURI,
-			Class:    dns.ClassINET,
-			Ttl:      36,
-			Rdlength: 100,
-		},
-		Priority: 1,
-		Weight:   1,
-		Target:   "http://192.168.10.252:5000/auth?&response_type=code&redirect_uri=http://192.168.10.252:9009/v/1&client_id=did.fuxi.city/jinzhongze@fxzk.org",
-	}
-	cert := dns.CERT{
-		Hdr: dns.RR_Header{
-			Name:     domain,
-			Rrtype:   dns.TypeCERT,
-			Class:    dns.ClassINET,
-			Ttl:      108,
-			Rdlength: 100,
-		},
-		Type:        1,
-		KeyTag:      1,
-		Algorithm:   1,
-		Certificate: "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVA2gkONVS5y3W6cnEaERuPsGNqD8kG14+sWjoquToTC7dlAIdNZzF3+a+Ak+bWaHZqF0f+3rVh/bD7uFO4riRyuRZVha09riDPFbsAb1S8//20v59dk+cfx0LskbUzne8U4dS0MCAA8TLhI/wnjJ0KAOeCAC8S1dFt0GwckOSlwIDAQAB",
-	}
-
-	results = append(results, &uri)
-	results = append(results, &cert)
-
-	return results, nil
+	return domains[name], nil
 }
 
 // Result of a lookup
