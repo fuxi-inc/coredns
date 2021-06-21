f94760b23ede0e72794dca4664c6b33ee3d67ab6
weijiuqi <weijiuqi1997@hotmail.com>
2021-06-21 11:31:39 +0800

    Add DoT log with unified format.

--- core/dnsserver/server_tls.go
+++ core/dnsserver/server_tls.go
@@ -4,11 +4,17 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"io"
 	"net"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/coredns/caddy"
+	"github.com/coredns/coredns/plugin/pkg/dnstest"
 	"github.com/coredns/coredns/plugin/pkg/reuseport"
 	"github.com/coredns/coredns/plugin/pkg/transport"
+	"github.com/coredns/coredns/request"
 
 	"github.com/miekg/dns"
 )
@@ -51,6 +57,7 @@ func (s *ServerTLS) Serve(l net.Listener) error {
 	s.server[tcp] = &dns.Server{Listener: l, Net: "tcp-tls", Handler: dns.HandlerFunc(func(w dns.ResponseWriter, r *dns.Msg) {
 		ctx := context.WithValue(context.Background(), Key{}, s.Server)
 		s.ServeDNS(ctx, w, r)
+		DoTLog(w, r)
 	})}
 	s.m.Unlock()
 
@@ -83,4 +90,65 @@ func (s *ServerTLS) OnStartupComplete() {
 	if out != "" {
 		fmt.Print(out)
 	}
+}
+
+// DoT Log handler with its items and format.
+func DoTLog(w dns.ResponseWriter, msg *dns.Msg) {
+	state := request.Request{W: w, Req: msg}
+	rrw := dnstest.NewRecorder(w)
+	//format := `{remote}:{port} ` + replacer.EmptyValue + ` {>id} "{type} {class} {name} {proto} {size} {>do} {>bufsize}" {rcode} {>rflags} {rsize} {duration}`
+	timestamp := strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
+	replace := make(map[string]string)
+	replace["{remote}:{port}"] = state.RemoteAddr()
+	replace["emptyValue"] = "-"
+	replace["id"] = strconv.FormatInt(int64(state.Req.Id), 10)
+	replace["type"] = state.Type()
+	replace["class"] = state.Class()
+	replace["name"] = state.Name()
+	replace["proto"] = state.Proto()
+	replace["size"] = strconv.FormatInt(int64(state.Req.Len()), 10)
+	replace["do"] = strconv.FormatBool(state.Do())
+	replace["bufsize"] = strconv.FormatInt(int64(state.Size()), 10)
+	if rrw == nil {
+		replace["rcode"] = "-"
+	} else if rcode := dns.RcodeToString[rrw.Rcode]; rcode != "" {
+		replace["rcode"] = rcode
+	} else {
+		replace["rcode"] = strconv.FormatInt(int64(rrw.Rcode), 10)
+	}
+	replace["rflags"] = "-"
+	if w == nil {
+		replace["rsize"] = "-"
+	} else {
+		replace["rsize"] = strconv.FormatInt(int64(rrw.Len), 10)
+	}
+	if rrw == nil {
+		replace["duration"] = "-"
+	} else {
+		secs := time.Since(rrw.Start).Seconds()
+		replace["duration"] = strconv.FormatFloat(secs, 'f', -1, 64) + "s"
+	}
+	//format := `{remote}:{port} ` + replacer.EmptyValue + ` {>id} "{type} {class} {name} {proto} {size} {>do} {>bufsize}" {rcode} {>rflags} {rsize} {duration}`
+	logstr := "[INFO] " + timestamp + " " + replace["{remote}:{port}"] + " " + replace["emptyValue"] + " " + replace["id"] + " \"" + replace["type"] + " " + replace["class"] + " " + replace["name"] + " " + replace["proto"] + " " + replace["size"] + " " + replace["do"] + " " + replace["bufsize"] + "\" " + replace["rcode"] + " " + replace["rflags"] + " " + replace["rsize"] + " " + replace["duration"]
+	WriteDoTLog(logstr)
+}
+
+// Write log in DoT log file.
+func WriteDoTLog(LogItem string) error {
+	filePath := "/home/fuxi/coredns/"
+	fileName := "coredns_dot.log"
+	lineFeed := "\r\n"
+	f, err := os.OpenFile(filePath+fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("DoT log file create failed. err: " + err.Error())
+		return err
+	} else {
+		_, err := io.WriteString(f, LogItem+lineFeed)
+		if err != nil {
+			fmt.Println("DoT log writing failed. err: " + err.Error())
+			return err
+		}
+	}
+	defer f.Close()
+	return nil
 }
